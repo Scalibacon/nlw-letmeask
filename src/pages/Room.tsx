8b03efc34885c49ae684f1a0ff60bfa3a5115451
ScaliTheus <scalibacon@gmail.com>
2021-06-23 23:05:01 -0300

    Criação da question list na room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,16 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { FormEvent, useState } from 'react';
 
 import { Button } from '../components/Button';
 import logoImg from '../assets/images/logo.svg';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
 
 import '../styles/room.scss';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
-import { useEffect } from 'react';
+
 
 type FirebaseQuestions = Record<string, {
   author: {
@@ -20,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   isHighlighted: boolean
 }>
 
-type Question = {
+type QuestionType = {
   id: string,
   author: {
     name: string,
@@ -39,7 +41,7 @@ export function Room(){
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState();
 
   const roomId = params.id;
@@ -128,7 +130,17 @@ export function Room(){
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+        {questions.map(question => {
+          return (
+            <Question
+              key={question.id}
+              content={question.content}
+              author={question.author}
+            />
+          )
+        })}
+        </div>        
       </main>
     </div>
   )
